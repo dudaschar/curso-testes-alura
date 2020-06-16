225510c7519cd53b4ed0f02ef4a1d48a3bc90d1f
Arthur Fücher <arthurfucher@gmail.com>
2020-06-16 20:01:47 -0300

    adiciona botao para esconder/mostrar saldo da conta

--- src/conta/Conta.js
+++ src/conta/Conta.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Conta = () => {
+    const [deveMostrarSaldo, mostraSaldo] = useState(true);
     const saldoConta = 1234.56;
     return <div>
         <h2>Conta</h2>
         <p>Saldo: {`R$ ${saldoConta.toString().replace('.',',')}`}</p>
+        <button onClick={() => mostraSaldo(!deveMostrarSaldo)}>{deveMostrarSaldo ? "Esconder saldo" : "Mostrar saldo"}</button>
     </div>
 }
 
--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Conta from './Conta';
 
 test('renderiza Conta', () => {
@@ -12,4 +12,21 @@ test('apresenta saldo da conta', () => {
   const { getByText } = render(<Conta />);
   const títuloDaConta = getByText('Saldo: R$ 1234,56');
   expect(títuloDaConta).toBeInTheDocument();
+});
+
+test('apresenta opção para não mostrar saldo da conta', () => {
+  const { getByText } = render(<Conta />);
+  const esconderSaldo = getByText('Esconder saldo');
+  expect(esconderSaldo).toBeInTheDocument();
+});
+
+test('opção de mostrar saldo aparece após esconder saldo', () => {
+  const { getByText } = render(<Conta />);
+  const esconderSaldo = getByText('Esconder saldo');
+
+  fireEvent.click(esconderSaldo, new MouseEvent('click'));
+
+  const mostrarSaldo = getByText('Mostrar saldo');
+
+  expect(mostrarSaldo).toBeInTheDocument();
 });
