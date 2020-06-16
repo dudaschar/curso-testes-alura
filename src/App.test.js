f48a31651f0a00b31bcf015a75eeb4c8ba7421d1
Arthur Fücher <arthurfucher@gmail.com>
2020-06-16 19:19:16 -0300

    limpa pagina principal e deixa titulo apenas

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders ByteBank title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(`ByteBank`);
   expect(linkElement).toBeInTheDocument();
 });
